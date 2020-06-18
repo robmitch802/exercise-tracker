b72104ea3554fb214982bf913590cc955ccd73cb
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-06-18 11:51:26 -0400

    Full project setup

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const ObjectId = require('mongodb').ObjectId
 
 require('dotenv').config();
 
@@ -9,17 +10,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+//using bodyParser for REST request reads
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
     );
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully.")
 });
 
+//sets up routes
 const exerciseRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
